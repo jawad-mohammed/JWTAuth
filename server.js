a7166d265ac7bc61343d0c35ea00a661bb2fd89d
Jawad Mohammed <80121274+jawad-mohammed@users.noreply.github.com>
2022-07-25 14:42:37 +0530

    refresh token and access token updated

--- server.js
+++ server.js
@@ -1,91 +1,93 @@
-
+//JWT auth,
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<JWT TOKEN code >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const express = require('express')
-const app = express()
-
-const cors = require('cors')
-const pool = require('./database')
 const bcrypt = require('bcrypt')
-const jwtGenerator = require('./utils/jwtHelpers')
-
-app.use(cors())
-app.use(express.json())
-//routes
-app.post('/registration', async (req, res) => {
-    try {
-        //verify the existing user from database
-        const verifyCustomer = await pool.query(`select * from customerdetailslogin where email = $1`,
-            [req.body.email])
-
-        if (verifyCustomer.rows.length > 0) return res.json('user already exists please try another one')
-
-        //Hasing password
-        //for password1
-        const salt = await bcrypt.genSalt(10)
-        const bcryptPassword = await bcrypt.hash(req.body.password, salt)
-        //for password2
-        const isSalt = await bcrypt.genSalt(10)
-        const bcryptConfirmPassword = await bcrypt.hash(req.body.confirmPassword, isSalt)
-
-        const newRegistration = await pool.query
-            (`insert into customerdetailslogin
-        (name,email,password,confirmpassword)values($1,$2,$3,$4) returning *`,
-                [req.body.name, req.body.email, bcryptPassword, bcryptConfirmPassword])
-        //jwt 
-        const jwtToken = jwtGenerator(newRegistration.rows[0].customerid)
-        return res.json({ jwtToken })
-
-
-    } catch (error) {
-        console.log(error);
-    }
-})
+const router = express.Router()
+const pool = require('../database')
+const jwtHelper = require('../utils/jwtHelpers')
 
 
 
-app.get('/registration', async (req, res) => {
-    const allUsers = await pool.query(`select * from customerdetailslogin `)
-    res.json(allUsers.rows)
+//register new user 
+router.post('/api/v1/register',async(req,res)=>{
+//check in database with email if exists then throw error
+const verifyUser = await pool.query(`select * from customerdetailslogin where email = $1`,
+[req.body.email])
+if(verifyUser.rows.length>0) return res.json(`user already exists`)
+//hash the password
+const salt = await bcrypt.genSalt(10)
+const hashedPassword = await bcrypt.hash(req.body.password,salt)
+
+    const newUser = await pool.query(`insert into customerdetailslogin(name,email,password,confirmpassword,phonenum)
+    values($1,$2,$3,$4,$5)returning *`,
+        [req.body.name,req.body.email,hashedPassword,req.body.confirmpassword,req.body.phonenum])
+        
+const jwtToken = jwtHelper(newUser.rows[0].userid)
+return res.json({jwtToken})
+
+
 
 })
 
+//returning user  take only useremai land password 
 
+router.post('/api/v1/login',async(req,res)=>{
+    // user must be stored in database
+       const checkUser = await pool.query(`select * from customerdetailslogin where email =$1`,[req.body.email])
+     if(checkUser.rows.length === 0) return res.json(`user not exists`)
 
-//
-//LOGIN ROUTE//
-app.post('/login', async (req, res) => {
-    try {
-        //verify whether user doesnot exist if so throw error
-        const checkUser = await pool.query(`select * from customerdetailslogin where email= $1`, [req.body.email])
-        if (checkUser.rows.length === 0) return res.json('sorry you are not authorized')
+    //get the useremail password and verify in database comapre user password with database password
+    const validPassword = await bcrypt.compare(req.body.password,checkUser.rows[0].password)
+   
+    
+    
+    
+    
+    //it verifies the otp with node generated otp
+//    const val = Math.floor(1000 + Math.random() * 9000);
+//     console.log(val);
+//      res.json(val)
+//      const verifyToken = (req.body.token=== 123)
+// if(!verifyToken) return res.json('its false token')
 
-        // verify the user has entered the same password as existed/stored in database
-        //checking the given password is equavalant to existing password
-        const validPassword = await bcrypt.compare(req.body.password,
-            checkUser.rows[0].password)
-
-        if (!validPassword) return res.json('wrong password')
-
-
-        // provide jwt token
-        const jwtToken = jwtGenerator(checkUser.rows[0].customerid)
-
-        return res.json({ jwtToken })
-
-
-
-
-    } catch (error) {
-        console.log(error);
-    }
+     if(!validPassword) return res.json(`invalid password`)
+    //if(!verifyToken) return res.json('invalid token') 
+    
+        const jwtToken = jwtHelper(checkUser.rows[0].userid)
+   return res.json({myJwtToken:jwtToken})
 })
 
 
 
 
 
+ //it verifies the otp with node generated otp
+ //it verifies the otp with node generated otp
+ var val = Math.floor(1000 + Math.random() * 9000);
+console.log(val);
+
+router.post('/api/v1/otp',(req,res)=>{
+//res.json(val)
+
+if(req.body.token === val){
+    return res.json(val)
+}
 
 
-
-app.listen(5000, () => {
-    console.log(`port is listening on 5000`);
 })
+
+
+
+
+
+
+router.get('/api/v1/get',async(req,res)=>{
+    const getAllUsers = await pool.query(`select * from customerdetailslogin`)
+   res.json(getAllUsers.rows)
+
+})
+
+
+
+module.exports = router
+
